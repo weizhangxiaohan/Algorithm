ed7e0525c98fe30579f3214609d6422a9344a51a
weizhangxiaohan_C <marvin.wei@praemium.com>
2016-05-16 18:29:11 +0800

    go

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -10,6 +10,11 @@ namespace QuickSort
     {
         static void Main(string[] args)
         {
+            int[] array = {1,4,6,2,5,9,7,3,8 };
+            Util.ArrayHelper.print<int>(array);
+            Array.Sort(array);
+            Util.ArrayHelper.print<int>(array);
+            Console.ReadKey();
         }
     }
 }
---
+++ Util/ArrayHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    public static class ArrayHelper
+    {
+        public static void print<T>(T[] array) 
+        {
+            string printStr = "";
+            Array.ForEach<T>(array, (T t) => { printStr += t.ToString() + ","; });
+            printStr = printStr.Trim(',');
+            Console.WriteLine(printStr); 
+        }
+    }
+}
